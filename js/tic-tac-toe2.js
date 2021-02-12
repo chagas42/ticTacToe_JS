760980f244b0d62d6c0ab7a457b15af770b2d360
Junior Chagas <jjuniorchagas15@gmail.com>
2021-02-12 20:56:44 -0300

    create folders/files from the game

---
+++ js/tic-tac-toe2.js
@@ -0,0 +1,144 @@
+const board = ['', '', '', '', '', '', '', '', '']; //
+const winning_sequences = [
+        
+    [0,1,2], //sequencia vencedora 0
+    [3,4,5], //sequencia vencedora 1
+    [6,7,8], //sequencia vencedora 2
+    [0,3,6], //sequencia vencedora 3
+    [1,4,7], //sequencia vencedora 4
+    [2,5,8], //sequencia vencedora 5
+    [0,4,8], //sequencia vencedora 6
+    [2,4,6]  //sequencia vencedora 7
+
+]; 
+const simbols = {
+    options:['X', 'O'], 
+    turn_index:0, 
+    change: function(){ 
+        this.turn_index = ((this.turn_index === 0) ? 1 : 0 ); 
+    }
+}; 
+let score_X = null; 
+let score_O = null; 
+let play_button_element = null; 
+let message_winning_element = null; 
+let container_element = null; 
+let gameover = false; 
+
+
+const init = ( element1, element2, element3, element4, element5 ) => { 
+    
+    container_element = element1; 
+    message_winning_element = element2; 
+    play_button_element = element3; 
+    score_O = element4; 
+    score_X = element5; 
+
+}; 
+
+const make_play = ( position ) => { 
+
+    if(gameover == true){
+        return false; 
+    };
+
+    if(board[position] === '' ){ 
+
+        board[position] = simbols.options[ simbols.turn_index ]; 
+        draw(); 
+        let winning_sequences_index = check_winning_sequences( simbols.options[ simbols.turn_index ] ); 
+
+        if( winning_sequences_index >= 0 ){
+
+            game_is_over(); 
+
+        } else { 
+
+            simbols.change(); 
+
+        }
+
+        return true; 
+
+    } else {
+
+        return false; 
+
+    }
+}; 
+
+
+function game_is_over(){ 
+
+    gameover = true; 
+    console.log('GAME OVER'); 
+
+};
+
+function restart(){ 
+
+    board.fill(''); 
+    message_winning_element.style.display = 'none'; 
+    draw(); 
+    gameover = false; 
+    document.querySelector('.message-placar-O').style.display = 'none'; 
+    document.querySelector('.message-placar-X').style.display = 'none'; 
+
+}; 
+
+
+function check_winning_sequences(simbol){ 
+
+    for(i in winning_sequences ){ 
+        
+        if( board [ winning_sequences[i][0] ] == simbol &&
+            board [ winning_sequences[i][1] ] == simbol &&
+            board [ winning_sequences[i][2] ] == simbol  ){ 
+                message_winning(simbol); 
+                return i; 
+            }
+
+    }; 
+    return -1; 
+
+}; 
+
+
+function message_winning(simbol){ //mostra o 
+
+    message_winning_element.style.display = 'block'; 
+    play_button_element.innerHTML = 'Restart'; 
+    write_winning(simbol); 
+
+}; 
+
+function write_winning(simbol){ //escreve na tela qual player foi vitorioso  
+    
+    let pontos_X = score_X.innerHTML; 
+    let pontos_O = score_O.innerHTML; 
+    if( simbol =='X' ){ 
+
+        document.querySelector('.message-placar-X').style.display = 'flex'; 
+        score_X.innerHTML = Number( pontos_X ) + 1; 
+    
+    } else { 
+
+        document.querySelector('.message-placar-O').style.display = 'flex'; 
+        score_O.innerHTML = Number( pontos_O ) + 1; 
+
+    }
+
+}; 
+
+
+function draw(){ //desenha a ação no nosso board atraves da função make_play
+
+    let content = ''; 
+    for( i in board){ 
+        
+        content += '<div onclick="make_play(' + i + ')">' + board[i] + '</div>'; 
+
+    }
+    container_element.innerHTML = content; 
+
+}; 
