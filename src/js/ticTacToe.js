54deda4db8a844aa5f8cbb2887729ece81d57042
Junior Chagas <jjuniorchagas15@gmail.com>
2021-02-12 21:21:14 -0300

    update spacing from the file:ticTacToe.js

--- src/js/ticTacToe.js
+++ src/js/ticTacToe.js
@@ -1,7 +1,6 @@
 const board = ['', '', '', '', '', '', '', '', '']; 
 
-const winning_sequences = [
-        
+const winning_sequences = [     
     [0,1,2], 
     [3,4,5], 
     [6,7,8], 
@@ -10,7 +9,6 @@ const winning_sequences = [
     [2,5,8],
     [0,4,8],
     [2,4,6] 
-
 ]; 
 
 const simbols = {
